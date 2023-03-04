c6376771e6a6cfb6defb37fa71d07e0b357bc422
AmmiMal <malyshkina.a.m@gmail.com>
2023-03-04 14:52:23 +0300

    Добавляем капитана

--- main.py
+++ main.py
@@ -10,13 +10,34 @@ def main():
     db_session.global_init("db/mars.db")
     #app.run()
     user = User()
-    user.surname = "Scott"
-    user.name = "Ridley"
-    user.age = 21
-    user.position = "captain"
-    user.speciality = "research engineer"
+    # user.surname = "Scott"
+    # user.name = "Ridley"
+    # user.age = 21
+    # user.position = "captain"
+    # user.speciality = "research engineer"
+    # user.address = "module_1"
+    # user.email = "scott_chief@mars.org"
+    # user.surname = "Vardey"
+    # user.name = "Max"
+    # user.age = 30
+    # user.position = "pilot"
+    # user.speciality = "explorer"
+    # user.address = "module_2"
+    # user.email = "max.var@mars.org"
+    # user.surname = "Black"
+    # user.name = "Bella"
+    # user.age = 23
+    # user.position = "captain's assistant"
+    # user.speciality = "geologist"
+    # user.address = "module_1"
+    # user.email = "bellabla@mars.org"
+    user.surname = "Armas"
+    user.name = "Nill"
+    user.age = 19
+    user.position = "doctor"
+    user.speciality = "therapist"
     user.address = "module_1"
-    user.email = "scott_chief@mars.org"
+    user.email = "doc_nill@mars.org"
     db_sess = db_session.create_session()
     db_sess.add(user)
     db_sess.commit()
